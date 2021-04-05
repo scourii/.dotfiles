593a12d9c55c0803d5f6e3f16f0581b33728952f
SugarBlank <eneblank19@protonmail.com>
2021-04-05 13:13:31 -0400

    Create audioup.sh

---
+++ scripts/audioup.sh
@@ -0,0 +1,20 @@
+volume_up="$(pactl set-sink-volume @DEFAULT_SINK@ +5%)"
+
+current_audio="$(pacmd list-sinks | grep -A 15 '* index'| awk '/volume: front/{ print $5 }' | sed 's/%//g')"
+
+if [ "$current_audio" = "0" ]; then
+        icon_name="/usr/share/icons/Papirus/16x16/panel/audio-volume-muted.svg"
+    else
+        if [ "$current_audio" -lt "30" ]; then
+            icon_name="/usr/share/icons/Papirus/16x16/panel/audio-volume-low.svg"
+        else
+            if [ "$current_audio" -lt "70" ]; then
+                icon_name="/usr/share/icons/Papirus/16x16/panel/audio-volume-medium.svg"
+            else
+                icon_name="/usr/share/icons/Papirus/16x16/panel/audio-volume-high.svg"
+            fi
+        fi
+fi
+
+notify-send "Volume: ${current_audio}%" -i ${icon_name} -h string:x-canonical-private-synchronous:anything
+
